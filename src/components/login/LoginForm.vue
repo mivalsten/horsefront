1af75d68be537ff14e1d09db9f33b554fdf1083a
Kornelia Kobiela <korneliak.95@gmail.com>
2021-03-02 21:23:20 +0100

    Some edit

--- src/components/login/LoginForm.vue
+++ src/components/login/LoginForm.vue
@@ -10,7 +10,24 @@
 export default {
     methods: {
         redirectToGoogle() {
-            window.location.replace("https://api.konwent.online/login/google");
+            this.$gAuth
+                .signIn()
+                .then((GoogleUser) => {
+                    // on success do something
+                    console.log("GoogleUser", GoogleUser);
+                    console.log("getId", GoogleUser.getId());
+                    console.log(
+                        "getBasicProfile",
+                        GoogleUser.getBasicProfile()
+                    );
+                    console.log(
+                        "getAuthResponse",
+                        GoogleUser.getAuthResponse()
+                    );
+                })
+                .catch((error) => {
+                    console.log("error", error);
+                });
         },
     },
 };
--- src/main.js
+++ src/main.js
@@ -1,11 +1,18 @@
 import { createApp } from "vue";
-import router from './router';
-import App from './App.vue';
-import installElementPlus from './plugins/element.js';
+import router from "./router";
+import App from "./App.vue";
+import GAuth from "vue3-google-oauth2";
+import installElementPlus from "./plugins/element.js";
 
 const app = createApp(App);
 installElementPlus(app);
 app.use(router);
+const gAuthOptions = {
+    clientId:
+        "513565002477-bmg6sjdr2gbtcf1eon15udvr0bjkgp9n.apps.googleusercontent.com",
+    scope: "email",
+    prompt: "consent",
+};
+app.use(GAuth, gAuthOptions);
 
 app.mount("#app");
-
