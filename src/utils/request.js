3f506041f02c19416b17bea24bd526a1bb4dfe82
Piotr Szmielew <p.szmielew@ava.waw.pl>
2021-03-02 23:58:58 +0100

    Send token to server

--- src/utils/request.js
+++ src/utils/request.js
@@ -2,42 +2,42 @@ import axios from "axios";
 export const API = "https://api.konwent.online/";
 
 const instance = axios.create({
-    baseURL: API,
-    headers: {
-        "Access-Control-Allow-Origin": "*",
-        "Content-Type": "application/json",
-    },
+  baseURL: API,
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+    "Content-Type": "application/json",
+  },
 });
 
 export default {
-    getEvents() {
-        return instance.get("/events");
-    },
-    getEvent(path) {
-        return instance.get("/events/" + path);
-    },
-    getUserProfile() {
-        return instance.get("/i_am");
-    },
-    updateUserProfile(data) {
-        return instance.post("/i_am/changing", data);
-    },
-    getAttendedEvents() {
-        return instance.get("/i_am/attending");
-    },
-    getOrganizedEvents() {
-        return instance.get("/i_am/organizing");
-    },
-    deleteUserAccount() {
-        return instance.delete("/i_am/removing/myself");
-    },
-    attendee(path) {
-        return instance.get(path + "/attendence");
-    },
-    unattendee(path) {
-        return instance.delete(path + "/attendence");
-    },
-    sentAuthCode(path) {
-        return instance.post(path);
-    },
+  getEvents() {
+    return instance.get("/events");
+  },
+  getEvent(path) {
+    return instance.get("/events/" + path);
+  },
+  getUserProfile() {
+    return instance.get("/i_am");
+  },
+  updateUserProfile(data) {
+    return instance.post("/i_am/changing", data);
+  },
+  getAttendedEvents() {
+    return instance.get("/i_am/attending");
+  },
+  getOrganizedEvents() {
+    return instance.get("/i_am/organizing");
+  },
+  deleteUserAccount() {
+    return instance.delete("/i_am/removing/myself");
+  },
+  attendee(path) {
+    return instance.get(path + "/attendence");
+  },
+  unattendee(path) {
+    return instance.delete(path + "/attendence");
+  },
+  sentAuthCode(path, data) {
+    return instance.post(path, data);
+  },
 };
