88ddc4527359f865f979e32714097ca2cabd60d9
Maciej Starzycki <zefir6@gmail.com>
2021-03-03 08:58:07 +0000

    Zmiana sciezki do api

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-export const API = "https://api.konwent.online/";
+export const API = "https://sesje.konwent.online/";
 
 const instance = axios.create({
     baseURL: API,
